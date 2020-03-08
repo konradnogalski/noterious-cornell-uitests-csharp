7809235572d3413f4849185ea6887a574c22ccad
Konrad <konradnogalski@gmail.com>
2020-03-08 08:14:37 +0100

    Login page smoke tests

---
+++ Tests/SmokeTests/LoginPageSmokeTests.cs
@@ -0,0 +1,63 @@
+using System;
+using PageObjects;
+using Infrastructure;
+using NUnit.Framework;
+using Infrastructure.PageObject.PageElements;
+using Infrastructure.PageObject.PageElements.Interfaces;
+
+namespace Tests
+{
+    public class LoginPageSmokeTests : BaseUITestTemplate
+    {
+        [Test]
+        public void SetUserName()
+        {
+            var testUserName = "username";
+            SetTextFieldSmokeTest((p) => p.UserName, testUserName);
+        }
+
+        [Test]
+        public void SetPassword()
+        {
+            var testPassword = "password";
+            SetTextFieldSmokeTest((p) => p.Password, testPassword);
+        }
+  
+        [Test]
+        public void ClickLoginButton()
+        {
+            ClickSmokeTest((p) => p.LoginButton);
+        }
+
+        [Test]
+        public void ClickGoogleButton()
+        {
+            ClickSmokeTest((p) => p.GoogleLoginButton);
+        }
+
+        [Test]
+        public void ClickRegisterLink()
+        {
+            ClickSmokeTest((p) => p.RegisterLink);
+        }
+
+        private void SetTextFieldSmokeTest(Func<LoginPageObject, TextField> textFieldSpecifier, string value)
+        {
+            TestSteps().
+                GoTo<LoginPageObject>((p, a) =>
+                {
+                    var textField = textFieldSpecifier.Invoke(p);
+                    a.Set(textFieldSpecifier.Invoke(p), value)
+                        .Assert(that => that
+                            .Field(textFieldSpecifier.Invoke(p), Is.EqualTo(value)));
+                });
+        }
+
+        private void ClickSmokeTest(Func<LoginPageObject, IClickable> buttonSpecifier)
+        {
+            TestSteps().
+                GoTo<LoginPageObject>((p, a) => a
+                    .Click(buttonSpecifier(p)));
+        }
+    }
+}
